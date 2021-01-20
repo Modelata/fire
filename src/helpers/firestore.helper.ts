774d15316e091065b0335c8a8aeeecab4fea444e
Alexis Chaves Pereira <alexis.moventes@gmail.com>
2021-01-20 14:58:06 +0100

    fix import

--- src/helpers/firestore.helper.ts
+++ src/helpers/firestore.helper.ts
@@ -1,3 +1,26 @@
+import { DocumentData } from '@google-cloud/firestore';
+
+export function convertDataFromDb(data: DocumentData): DocumentData {
+  if (data) {
+    for (const key in data) {
+      if (data.hasOwnProperty(key) && data[key]) {
+
+        if (typeof (data[key] as any).toDate === 'function') {
+          // attribute is a Firebase Timestamp
+          data[key] = (data[key] as any).toDate();
+
+        } else if (typeof (data[key] as any) === 'object') {
+          // attribute is an object or an array
+          if (Object.keys(data[key]).length > 0 && !isDocumentReference(data[key])) {
+            data[key] = convertDataFromDb(data[key]);
+          }
+        }
+      }
+    }
+  }
+  return data;
+}
+
 export function isDocumentReference(data: any): boolean {
   return !!(data && data.id && data.parent && data.path && data.firestore);
 }
